0ce07e404b166a18874c89727b01412e23a13333
Jake Thrasher <mjakethrasher@gmail.com>
2021-06-08 10:56:02 -0700

    create boxes on mouse drag

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -1,34 +1,44 @@
+/* eslint-disable max-len */
 import React, { createRef, useEffect } from 'react';
+import Matter from 'matter-js';
 import p5 from 'p5';
+export const Engine = Matter.Engine;
+export const Runner = Matter.Runner;
+export const Bodies = Matter.Bodies;
+export const Composite = Matter.Composite;   
+export const engine = Engine.create();
+export const runner = Runner.create();
+engine.gravity.y = - 0.05;
+import { Box } from '../matter js/box';
 
 export const SketchComponent = () => {
     const myRef = createRef();
-
-    const Sketch = (p) => {
-        p.setup = () => {
-            p.createCanvas(800, 800);
-        };
-        let xS = 0;
-        let yS = 0;
-
-        p.draw = () => {
-            p.background(0);
-            p.colorMode(p.HSB);
-            p.fill(yS / 4, 255, 255);
-            p.noStroke();
-            p.noCursor();
-            p.ellipse(p.mouseX, p.mouseY, xS);
-            if(xS > 800) xS = 0;
-            if(yS > 800) yS = 0;
-            xS++;
-            yS++;
-        };
-    };
-
+    const boxes = [];
+    
     useEffect(() => {
         const myp5 = new p5(Sketch, myRef.current);
         return myp5;
     }, []);
+   
+    const Sketch = (p) => {
+        p.setup = () => {
+            p.createCanvas(800, 800);
+            Runner.run(runner, engine); 
+        };
+            
+        p.mouseDragged = () => {
+            boxes.push(new Box(p.mouseX, p.mouseY, 20, 20)); 
+        };
+
+        p.draw = () => {
+            p.background(0);
+            p.fill('#F8B195');
+            
+            for(let i = 0; i < boxes.length; i++){
+                boxes[i].show(p);
+            }
+        };  
+    };
 
     return <div ref={myRef}></div>;
 };
